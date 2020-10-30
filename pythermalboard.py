f3e112d382720f6c47acdbcf86456b762a382e2b
Tony <hyperkvmx86@gmail.com>
2020-10-30 00:19:21 -0400

    Revert "add NVIDIA GPU detection to show if temp is higher than CPU (more"
    Fuck polling nvidia-smi

--- pythermalboard.py
+++ pythermalboard.py
@@ -16,27 +16,16 @@ f=open("/sys/devices/platform/faustus/kbbl/kbbl_mode", 'w')
 if f != "0":
     f.write("0")
 
-# fucking NVIDIA
-present = os.popen("xrandr --listproviders | grep -o \"NVIDIA.*\"").read()
-
 while True:
     try:
         cputemp = open("/sys/class/thermal/thermal_zone0/temp", 'r').read().splitlines()[0]
-        if present:
-            gputemp = os.popen("nvidia-smi | awk '{print $3}' | xargs | awk '{print $7}' | sed 's/C//'").read()
-            gputemp = gputemp.split()[0]
-        else:
-            gputemp = 0
         cputemp = cputemp[:2]
-        if int(cputemp) >= 50 or int(gputemp) >= 50:
-            if int(cputemp) > int(gputemp):
-                redbase = (int(cputemp) - 50) * 10
-            elif int(cputemp) < int(gputemp):
-                redbase = (int(gputemp) - 50) * 10
+        if int(cputemp) >= 50:
+            redbase = (int(cputemp) - 50) * 10
             if redbase <= 255 and redbase > 0:
                 rval = hex(redbase)
                 if debug:
-                    print("rval: %s" % rval, cputemp, gputemp)
+                    print("rval: %s" % rval, cputemp)
                 r=open(red, 'w')
                 r.write(rval)
             elif redbase > 255:
@@ -44,59 +33,50 @@ while True:
                     print("rval: ff")
                 r=open(red, 'w')
                 r.write("ff")
-        elif int(cputemp) <= 50 and int(gputemp) <= 50:
+        elif int(cputemp) <= 50:
             r=open(red, 'w')
             r.write("00")
             if debug:
                 print("rval: 00")
 
-        if int(cputemp) <= 95 or int(gputemp) <= 85:
-            if int(cputemp) > int(gputemp):
-                incgreenbase = (int(cputemp) - 95) * -10
-            elif int(cputemp) < int(gputemp):
-                incgreenbase = (int(gputemp) - 85) * -10
+        if int(cputemp) <= 95:
+            incgreenbase = (int(cputemp) - 95) * -10
             if incgreenbase <= 255 and incgreenbase > 0:
                 incgval = hex(incgreenbase)
                 if debug:
-                    print("incgval: %s" % incgval, cputemp, gputemp)
+                    print("incgval: %s" % incgval, cputemp)
                 ig=open(green, 'w')
                 ig.write(incgval)
             elif incgreenbase > 255:
                 ig=open(green, 'w')
                 ig.write("ff")
-        elif int(cputemp) >= 85 and int(gputemp) >= 85:
+        elif int(cputemp) >= 85:
             ig=open(green, 'w')
             ig.write("00")
             if debug:
                 print("incgval: 00")
 
-        if int(cputemp) >= 20 or int(cputemp) <= 50 and int(gputemp) >= 20 or int(gputemp) <= 50:
-            if int(cputemp) > int(gputemp):
-                decgreenbase = (int(cputemp) - 20) * 10
-            elif int(cputemp) < int(gputemp):
-                decgreenbase = (int(gputemp) - 20) * 10
+        if int(cputemp) >= 20 and int(cputemp) <= 50:
+            decgreenbase = (int(cputemp) - 20) * 10
             if decgreenbase <= 255 and decgreenbase > 0:
                 decgval = hex(decgreenbase)
                 if debug:
-                    print("decgval %s" % decgval, cputemp, gputemp)
+                    print("decgval %s" % decgval, cputemp)
                 dg=open(green, 'w')
                 dg.write(decgval)
 
-        if int(cputemp) <= 50 or int(gputemp) <= 50:
-            if int(cputemp) > int(gputemp):
-                bluebase = (int(cputemp) - 50) * -10
-            elif int(cputemp) < int(gputemp):
-                bluebase = (int(gputemp) - 50) * -10
+        if int(cputemp) <= 50:
+            bluebase = (int(cputemp) - 50) * -10
             if bluebase <= 255 and bluebase > 0:
                 bval = hex(bluebase)
                 if debug:
-                    print("bval: %s" % bval, cputemp, gputemp)
+                    print("bval: %s" % bval, cputemp)
                 b=open(blue, 'w')
                 b.write(bval)
             elif bluebase > 255:
                 b=open(blue, 'w')
                 b.write("ff")
-        elif int(cputemp) >= 50 or int(gputemp) >= 40:
+        elif int(cputemp) >= 50:
             b=open(blue, 'w')
             b.write("00")
             if debug:
